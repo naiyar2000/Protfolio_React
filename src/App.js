a2b5ca935d5a201080dcfafa6afd7b69acd068de
naiyar.2000 <naiyarimam.2000@gmail.com>
2020-06-07 19:48:09 +0530

    Added link to expensetracker in protfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { LandingPage } from './components/LandingPage';
-// import { ExpenseTracker } from './components/ExpenseTracker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { ExpenseTracker } from './components/ExpenseTracker';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-        // <Router>
-        // <div>
-        //     <Switch>
-        //       <Route  exact path="/Portfolio" component={LandingPage} />
-        //       <Route  path="/Portfolio/ExpenseTracker" component={ExpenseTracker}/>
-        //     </Switch>
-        // </div>          
-        // </Router>
         <Router>
-          <LandingPage />
+        <div>
+            <Switch>
+              <Route  exact path="/" component={LandingPage} />
+              <Route  path="/ExpenseTracker" component={ExpenseTracker}/>
+            </Switch>
+        </div>          
         </Router>
+        // <Router>
+        //   <LandingPage />
+        // </Router>
   );
 }
 
--- src/components/LandingPageComponent/Portfolio.js
+++ src/components/LandingPageComponent/Portfolio.js
@@ -7,7 +7,7 @@ export const Portfolio = (props) => {
         <section id="Portfolio">
             <div className="portfolio_container">
                     <div><Link to="/">Home</Link></div>
-                    <div><Link to="/Portfolio/ExpenseTracker">Home</Link></div>
+                    <div><Link to="/ExpenseTracker">Home</Link></div>
                     <div><Link to="/">Home</Link></div>
                     <div><Link to="/">Home</Link></div>
             </div>
